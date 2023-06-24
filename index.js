afbc3a3508cbd813c2802b4564009590c299b1f7
mohitrathore7 <95867522+mohitrathore7@users.noreply.github.com>
2023-06-24 23:51:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var numberofdrums = document.querySelectorAll(".drum").length;
+for(var i = 0;i<numberofdrums;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+       
+     makesound(this.innerHTML);
+     designthebutton(this.innerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+   
+    makesound(event.key);
+    designthebutton(event.key);
+});
+
+function makesound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "a":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+        
+        case "s":
+        var audio = new Audio("sounds/snare.mp3");
+         audio.play();
+         break;
+
+         case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+            
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        default:
+      }
+}
+ 
+function designthebutton(key){
+    var activebutton = document.querySelector("."+key);
+    activebutton.classList.add("pressed");
+    setTimeout(() => {
+     activebutton.classList.remove("pressed");
+    }, 100);
+}
